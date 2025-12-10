35f0fb49eb9f339e55e0811afbcc1f19d7873395
nekrochan <rubus.saxatilis.99@gmail.com>
2025-12-10 15:42:53 +0300

    лаб7 безобидные изменения

--- app/src/main/java/com/example/succulentus/database/CharacterDatabase.kt
+++ app/src/main/java/com/example/succulentus/database/CharacterDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.succulentus.data.Character
-import com.rut.roomexample.entities.Character
+import com.example.succulentus.database.CharacterDao
 
 @Database(entities = [Character::class], version = 1)
 abstract class CharacterDatabase : RoomDatabase() {
