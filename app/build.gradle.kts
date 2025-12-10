cde8fe467c557ec23d4d3fc437a1bd6259105fef
nekrochan <rubus.saxatilis.99@gmail.com>
2025-12-10 16:53:27 +0300

    лаб7 не может сохранить в бд

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,15 +74,34 @@ dependencies {
     implementation("io.ktor:ktor-client-serialization:2.3.4")
     implementation("io.ktor:ktor-client-content-negotiation:2.3.4")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.4")
+    implementation ("androidx.datastore:datastore-preferences:1.1.6")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
 
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    val room_version = "2.8.4"
 
+    kapt("androidx.room:room-compiler:$room_version")
 
-    implementation ("androidx.room:room-runtime:2.6.0")
-    implementation ("androidx.room:room-ktx:2.6.0")
-    kapt ("androidx.room:room-compiler:2.6.0")
+    implementation("androidx.room:room-runtime:$room_version")
 
-    implementation ("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    // If this project only uses Java source, use the Java annotationProcessor
+    // No additional plugins are necessary
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+
+    // optional - RxJava2 support for Room
+    implementation("androidx.room:room-rxjava2:$room_version")
+
+    // optional - RxJava3 support for Room
+    implementation("androidx.room:room-rxjava3:$room_version")
+
+    // optional - Guava support for Room, including Optional and ListenableFuture
+    implementation("androidx.room:room-guava:$room_version")
+
+    // optional - Test helpers
+    testImplementation("androidx.room:room-testing:$room_version")
+
+    // optional - Paging 3 Integration
+    implementation("androidx.room:room-paging:$room_version")
 }
